5a1af14eac4f4bf6427bfca10f3a37e3c748eaba
Adam Lee <adam@codelabx.ltd>
2023-04-10 01:54:10 +0100

    decode query params

--- src/Restify/Presenters/PresenterResource.php
+++ src/Restify/Presenters/PresenterResource.php
@@ -5,13 +5,21 @@ namespace XtendLunar\Addons\RestifyApi\Restify\Presenters;
 use Binaryk\LaravelRestify\Getters\Getter;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use Binaryk\LaravelRestify\Repositories\Repository as RestifyRepository;
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Support\Str;
 
 abstract class PresenterResource
 {
+    protected mixed $queryParams = [];
+
     public function __construct(
         protected RestifyRepository $repository,
         protected mixed $data = []
-    ) {}
+    ) {
+        if (request()->has('params') && Str::isJson(request()->params)) {
+            $this->queryParams = Json::decode(request()->params);
+        }
+    }
 
     public static function fromData(RestifyRepository $repository, mixed $data): static
     {
