e0b4dfdb66bb3125d41cf03ff4d58022d011bdaa
Adam Lee <adam@codelabx.ltd>
2023-04-11 02:04:48 +0100

    translate category tree name

--- src/Resources/CategoryResource.php
+++ src/Resources/CategoryResource.php
@@ -18,7 +18,7 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'active' => (bool)($this->legacy_data['active'] ?? true),
-            'name' => $this->attribute_data['name'] ?? null,
+            'name' => $this->resource->translateAttribute('name') ?? null,
             'count' => DB::table('lunar_collection_product')->where('collection_id', $this->id)->count(),
             'children' => CategoryResource::collection($this->children),
         ];
