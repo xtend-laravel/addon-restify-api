51e57feff96433b35d13c97fae23071b2ab2b8da
bangnokia <bangnokia@gmail.com>
2023-04-27 05:29:04 +0700

    update add to cart api

--- src/Restify/Actions/UpdateCartAction.php
+++ src/Restify/Actions/UpdateCartAction.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
 use Lunar\Base\Purchasable;
 use Lunar\Models\Cart;
+use Lunar\Models\ProductOption;
 use Lunar\Models\ProductVariant;
 
 class UpdateCartAction extends Action
@@ -15,6 +16,7 @@ class UpdateCartAction extends Action
     public function handle(ActionRequest $request, Cart $cart): JsonResponse
     {
         $purchasable = $this->getPurchasable($request->product);
+
         $cart->add(
             purchasable: $purchasable,
             quantity: $request->product['quantity'] ?? 1,
@@ -23,20 +25,29 @@ class UpdateCartAction extends Action
         // @todo return any validation stock errors or any other errors when adding lines to cart
         return data($cart->lines->groupBy('purchasable_id')->get($purchasable->id)->flatMap(function ($line) {
             return [
-                'id' => $line->purchasable->product_id,
+                'id'          => $line->purchasable->product_id,
                 'purchasable' => $line->purchasable,
-                'quantity' => $line->quantity,
-                'total' => $line->total->value,
+                'quantity'    => $line->quantity,
+                'total'       => $line->total->value,
             ];
         }));
     }
 
     protected function getPurchasable(array $product): Purchasable|Model
     {
-        // @todo Check if product is a variant otherwise return base variant
-        return ProductVariant::query()->where([
-            'product_id' => $product['id'],
-            'base' => true,
-        ])->sole();
+        // when the product has no variants, we can just return the base variant
+        if (!isset($product['variants']) || blank($product['variants'])) {
+            return ProductVariant::query()->where([
+                'product_id' => $product['id'],
+                'base'       => true,
+            ])->sole();
+        }
+
+        return ProductVariant::query()
+            ->where(['product_id' => $product['id'], 'base' => false])
+            ->get()
+            ->first(
+                fn(ProductVariant $variant) => $variant->values->pluck('id')->diff(array_values($product['variants']))->isEmpty()
+            );
     }
 }
