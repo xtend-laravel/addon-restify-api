c5545a7c5fbee32ba5d9b0653d9b6c8e7df728e7
Adam Lee <adam@codelabx.ltd>
2023-04-29 23:12:53 +0100

    Allow search by SKU

--- src/Restify/Filters/Product/KeywordFilter.php
+++ src/Restify/Filters/Product/KeywordFilter.php
@@ -14,6 +14,8 @@ class KeywordFilter extends MatchFilter
     public function filter(RestifyRequest $request, Relation|Builder $query, $value)
     {
         $query->whereRaw('LOWER(JSON_EXTRACT(attribute_data, "$.name")) LIKE ?', ['%' . strtolower($value) . '%']);
+        $query->orWhereRaw('LOWER(JSON_EXTRACT(legacy_data, "$.sku")) LIKE ?', ['%' . strtolower($value) . '%']);
+
         // @todo do we need to search by description or any other fields?
         return $query;
     }
