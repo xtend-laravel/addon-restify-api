e294af92ffc8f2baa3d05718752e75b7892980bf
Adam Lee <adam@codelabx.ltd>
2023-04-30 15:46:09 +0100

    Custom routes for account sections WIP

--- src/Restify/CustomerRepository.php
+++ src/Restify/CustomerRepository.php
@@ -2,17 +2,21 @@
 
 namespace XtendLunar\Addons\RestifyApi\Restify;
 
-use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
+use Illuminate\Routing\Router;
 use Lunar\Models\Customer;
+use XtendLunar\Addons\RestifyApi\Controllers\AccountController;
+use XtendLunar\Addons\RestifyApi\Restify\Presenters\CustomerPresenter;
 
 class CustomerRepository extends Repository
 {
     public static string $model = Customer::class;
 
-    public function fields(RestifyRequest $request): array
+    public static string $presenter = CustomerPresenter::class;
+
+    public static function routes(Router $router, $attributes = [], $wrap = true)
     {
-        return [
-            id(),
-        ];
+        $router->group(['namespace' => '\XtendLunar\Addons\RestifyApi'], function (Router $router) {
+            $router->get('/account/{section?}', AccountController::class)->name('account');
+        });
     }
 }
