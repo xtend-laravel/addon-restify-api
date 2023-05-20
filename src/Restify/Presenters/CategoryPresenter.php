aaaed127c6b82b27a696ee62c43f8a1c66f86624
Boris Lepikhin <boris@lepikhin.com>
2023-05-19 20:52:37 -0700

    Return image_url for the collection thumbnail for categories

--- src/Restify/Presenters/CategoryPresenter.php
+++ src/Restify/Presenters/CategoryPresenter.php
@@ -9,11 +9,15 @@ class CategoryPresenter extends PresenterResource implements Presentable
 {
     public function transform(RestifyRequest $request): array
     {
+        $collection = $this->repository->model();
+        $imageSize = str_contains($collection->thumbnail?->getUrl(), 'fra1.digitaloceanspaces') ? 'medium' : '';
+
         return [
             'id' => $this->data['id'],
             'name' => $this->data['attribute_data']['name'],
             'description' => $this->data['attribute_data']['description'] ?? null,
             'filters' => $this->getter($request, 'filter-groups'),
+            'image_url' => $collection->thumbnail?->getUrl($imageSize) ?? null,
         ];
     }
 }
