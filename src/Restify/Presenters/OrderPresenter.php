34947a6bc52da0aa0416b00e3c9b9923e4af662e
Boris Lepikhin <boris@lepikhin.com>
2023-05-22 13:07:16 -0700

    Updated OrderResource

--- src/Restify/Presenters/OrderPresenter.php
+++ src/Restify/Presenters/OrderPresenter.php
@@ -3,8 +3,8 @@
 namespace XtendLunar\Addons\RestifyApi\Restify\Presenters;
 
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
-use XtendLunar\Addons\RestifyApi\Restify\Contracts\Presentable;
 use Lunar\Models\OrderLine;
+use XtendLunar\Addons\RestifyApi\Restify\Contracts\Presentable;
 use XtendLunar\Addons\RestifyApi\Restify\OrderRepository;
 
 class OrderPresenter extends PresenterResource implements Presentable
@@ -13,7 +13,7 @@ class OrderPresenter extends PresenterResource implements Presentable
     {
         return [
             'id' => $this->data->id,
-            'products' => $this->data->lines->transform(function(OrderLine $line) use ($request) {
+            'products' => $this->data->lines->transform(function (OrderLine $line) use ($request) {
                 $line->purchasable->load('values.option');
 
                 return OrderLinePresenter::fromData(
@@ -22,6 +22,7 @@ class OrderPresenter extends PresenterResource implements Presentable
                 )->transform($request);
             }),
             'total' => $this->data->total->value,
+            'status' => $this->data->status,
             // @todo do we format the date on the frontend?
             'created_at' => $this->data->created_at->format('m/d/Y'),
         ];
