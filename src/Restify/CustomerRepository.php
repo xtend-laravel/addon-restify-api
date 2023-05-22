0d77d12757c76df409576cb61d2a27a285cc8212
Boris Lepikhin <boris@lepikhin.com>
2023-05-22 14:26:17 -0700

    Customer order details endpoint

--- src/Restify/CustomerRepository.php
+++ src/Restify/CustomerRepository.php
@@ -5,6 +5,7 @@ namespace XtendLunar\Addons\RestifyApi\Restify;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Routing\Router;
 use Lunar\Models\Customer;
+use XtendLunar\Addons\RestifyApi\Resources\Customer\OrderResource;
 use XtendLunar\Addons\RestifyApi\Restify\Presenters\CustomerPresenter;
 
 class CustomerRepository extends Repository
@@ -16,9 +17,15 @@ class CustomerRepository extends Repository
     public static function routes(Router $router, $attributes = [], $wrap = true): void
     {
         $router->group(['namespace' => '\XtendLunar\Addons\RestifyApi'], function (Router $router) {
+            // @todo please check if there's a better way to pass an id down to the resource
+            $router->get('/account/orders/{order}', function ($order) {
+                return OrderResource::make(request()->all());
+            });
+
             $router->get('/account/{section?}', function (string $section = null) {
                 /** @var JsonResource|string $resource */
                 $resource = 'XtendLunar\Addons\RestifyApi\Resources\Customer\\'.ucfirst($section ?? 'Dashboard').'Resource';
+
                 abort_unless(class_exists($resource), 404);
 
                 return $resource::make(request()->all());
