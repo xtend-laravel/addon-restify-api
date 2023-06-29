c64be7586326741def22a212ea3cb93b173e2221
Adam Lee <adam@codelabx.ltd>
2023-06-29 22:30:59 +0100

    Fix category tree query to take the top level category

--- src/Restify/Getters/Lunar/CategoriesTreeGetter.php
+++ src/Restify/Getters/Lunar/CategoriesTreeGetter.php
@@ -5,6 +5,7 @@ namespace XtendLunar\Addons\RestifyApi\Restify\Getters\Lunar;
 use Binaryk\LaravelRestify\Getters\Getter;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use Illuminate\Http\JsonResponse;
+use Lunar\Models\CollectionGroup;
 use Symfony\Component\HttpFoundation\Response;
 use Xtend\Extensions\Lunar\Core\Models\Collection;
 use XtendLunar\Addons\RestifyApi\Resources\CategoryResource;
@@ -15,11 +16,15 @@ class CategoriesTreeGetter extends Getter
 
     public function handle(RestifyRequest $request, Collection $model = null): JsonResponse|Response
     {
+        $categoryHandle = CollectionGroup::query()->where('handle', 'categories')->sole();
         return response()->json([
             'data' => $model
                 ? CategoryResource::make($model)
                 : CategoryResource::collection(
-                    resource: Collection::query()->where('_lft', 1)->get(),
+                    resource: Collection::query()
+                        ->where('collection_group_id', $categoryHandle->id)
+                        ->whereNull('parent_id')
+                        ->get(),
                 ),
         ]);
     }
