d4af6c759a068f2449f88b39832e329badb3c3d6
Adam Lee <adam@codelabx.ltd>
2023-09-11 14:35:05 +0100

    fix: Order required to authorize payment

--- src/Restify/Actions/AuthorizePaymentCheckoutAction.php
+++ src/Restify/Actions/AuthorizePaymentCheckoutAction.php
@@ -7,6 +7,7 @@ use Binaryk\LaravelRestify\Http\Requests\ActionRequest;
 use Illuminate\Http\JsonResponse;
 use Lunar\Facades\Payments;
 use Lunar\Models\Cart;
+use Lunar\Models\Order;
 use Lunar\PaymentTypes\AbstractPayment;
 
 class AuthorizePaymentCheckoutAction extends Action
@@ -14,10 +15,20 @@ class AuthorizePaymentCheckoutAction extends Action
     public function handle(ActionRequest $request, Cart $models): JsonResponse
     {
         $cart = $models;
+        $order = Order::find($request->orderId);
+
+        if (! $order) {
+            return response()->json([
+                'message' => __('Order :id not found.', ['id' => $request->orderId]),
+            ], 422);
+        }
 
         /** @var AbstractPayment $paymentDriver */
         $paymentDriver = Payments::driver($request->paymentDriver ?? 'stripe');
-        $paymentDriver->cart($cart)->withData($request->all());
+        $paymentDriver
+            ->cart($cart)
+            ->order($order)
+            ->withData($request->all());
 
         try {
             $paymentStatus = $paymentDriver->init()->authorize();
