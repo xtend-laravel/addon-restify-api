aec911be9e550a65d0c4272dd8387915163cd1a5
Adam Lee <adam@codelabx.ltd>
2023-09-27 13:57:01 +0100

    chore: Collection slug

--- src/Restify/Presenters/CollectionPresenter.php
+++ src/Restify/Presenters/CollectionPresenter.php
@@ -3,6 +3,8 @@
 namespace XtendLunar\Addons\RestifyApi\Restify\Presenters;
 
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
+use Illuminate\Support\Str;
+use Lunar\Models\Url;
 use XtendLunar\Addons\RestifyApi\Restify\Contracts\Presentable;
 
 class CollectionPresenter extends PresenterResource implements Presentable
@@ -12,6 +14,7 @@ class CollectionPresenter extends PresenterResource implements Presentable
         return [
             'id' => $this->data['id'],
             'name' => $this->data['attribute_data']['name'],
+            'slug' => $this->data['id'].'-'.Str::slug($this->data['attribute_data']['name']),
             'description' => $this->data['attribute_data']['description'] ?? null,
             'images' => $this->getter($request, 'collection-image'),
         ];
