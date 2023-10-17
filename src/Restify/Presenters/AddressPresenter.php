b5abdf5098759d922c71e05da075aa633b78de52
Adam Lee <adam@codelabx.ltd>
2023-10-17 01:53:54 +0100

    chore: Append country iso

--- src/Restify/Presenters/AddressPresenter.php
+++ src/Restify/Presenters/AddressPresenter.php
@@ -3,12 +3,15 @@
 namespace XtendLunar\Addons\RestifyApi\Restify\Presenters;
 
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
+use Lunar\Models\Country;
 use XtendLunar\Addons\RestifyApi\Restify\Contracts\Presentable;
 
 class AddressPresenter extends PresenterResource implements Presentable
 {
     public function transform(RestifyRequest $request): array
     {
+        $this->data['country_iso'] = Country::find($this->data['country_id'])->iso2;
+
         return $this->data;
     }
 }
