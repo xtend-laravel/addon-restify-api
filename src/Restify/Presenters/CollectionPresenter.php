981b79ff65e2f20aac5e1d5a7f810fee45063fc0
Adam Lee <adam@codelabx.ltd>
2023-11-14 01:10:49 +0000

    fix: sub_heading collection

--- src/Restify/Presenters/CollectionPresenter.php
+++ src/Restify/Presenters/CollectionPresenter.php
@@ -4,7 +4,6 @@ namespace XtendLunar\Addons\RestifyApi\Restify\Presenters;
 
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use Illuminate\Support\Str;
-use Lunar\Models\Url;
 use XtendLunar\Addons\RestifyApi\Restify\Contracts\Presentable;
 
 class CollectionPresenter extends PresenterResource implements Presentable
@@ -14,6 +13,7 @@ class CollectionPresenter extends PresenterResource implements Presentable
         return [
             'id' => $this->data['id'],
             'name' => $this->data['attribute_data']['name'],
+            'sub_heading' => $this->data['attribute_data']['sub_heading'] ?? null,
             'slug' => $this->data['id'].'-'.Str::slug($this->data['attribute_data']['name']),
             'description' => $this->data['attribute_data']['description'] ?? null,
             'images' => $this->getter($request, 'collection-image'),
