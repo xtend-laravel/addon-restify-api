7347387921dd18a3ff60bf9979ac8d4382c05e20
Adam Lee <adam@codelabx.ltd>
2023-12-05 02:29:42 +0000

    feat: Send production orders to zapier

--- src/Restify/Actions/CreateOrderAction.php
+++ src/Restify/Actions/CreateOrderAction.php
@@ -4,8 +4,12 @@ namespace XtendLunar\Addons\RestifyApi\Restify\Actions;
 
 use Binaryk\LaravelRestify\Actions\Action;
 use Binaryk\LaravelRestify\Http\Requests\ActionRequest;
+use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
+use Binaryk\LaravelRestify\Repositories\Repository as RestifyRepository;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
 use Lunar\Models\Cart;
+use XtendLunar\Addons\RestifyApi\Restify\Presenters\OrderPresenter;
 
 class CreateOrderAction extends Action
 {
@@ -34,8 +38,22 @@ class CreateOrderAction extends Action
             ], 422);
         }
 
+        if (app()->environment('production')) {
+            $this->sendOrderToZapier($order);
+        }
+
         return data([
             'order' => $order,
         ]);
+    }
+
+    protected function sendOrderToZapier($order): void
+    {
+        $orderData = OrderPresenter::fromData(
+            repository: RestifyRepository::resolveWith($order),
+            data: $order,
+        )->transform(new RestifyRequest(request()->all()));
+
+        Http::post('https://hooks.zapier.com/hooks/catch/17025856/3zleke8/', $orderData);
     }
 }
