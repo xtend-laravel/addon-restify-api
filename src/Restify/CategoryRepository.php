2c6660ce54a01a27fa4aa2cab44e00cce4b6b23b
Adam Lee <adam@codelabx.ltd>
2023-12-28 16:22:12 +0000

    fix: Category by name

--- src/Restify/CategoryRepository.php
+++ src/Restify/CategoryRepository.php
@@ -4,7 +4,9 @@ namespace XtendLunar\Addons\RestifyApi\Restify;
 
 use Binaryk\LaravelRestify\Fields\HasMany;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
+use Illuminate\Support\Str;
 use Xtend\Extensions\Lunar\Core\Models\Collection;
+use Xtend\Extensions\Lunar\Core\Models\ProductVariant;
 use XtendLunar\Addons\RestifyApi\Restify\Actions\GetCategoryProducts;
 use XtendLunar\Addons\RestifyApi\Restify\Getters\Lunar\CategoriesTreeGetter;
 use XtendLunar\Addons\RestifyApi\Restify\Getters\Lunar\FilterGroupsGetter;
@@ -21,6 +23,16 @@ class CategoryRepository extends Repository
     public static array $excludeFields = ['_lft', '_rgt'];
 
     public static bool|array $public = true;
+
+    protected static function booting(): void
+    {
+        $repositoryId = request()->route()->parameter('repositoryId');
+        if (is_string($repositoryId)) {
+            if ($repository = \Lunar\Models\Collection::query()->firstWhere('attribute_data->name->value->en', Str::headline($repositoryId))) {
+                request()->route()->setParameter('repositoryId', $repository->id);
+            }
+        }
+    }
 
     public static function related(): array
     {
